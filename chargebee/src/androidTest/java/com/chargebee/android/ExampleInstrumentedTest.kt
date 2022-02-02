1e3fd5ae6ed28cad7e78e6c9bf9779763522a083
cb-amutha <amutha.c@chargebee.com>
2022-02-02 17:01:10 +0530

    Test coverage sonar properties.

--- chargebee/src/androidTest/java/com/chargebee/android/ExampleInstrumentedTest.kt
+++ chargebee/src/androidTest/java/com/chargebee/android/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.test.chargebee.test", appContext.packageName)
+        assertEquals("com.chargebee.android.test", appContext.packageName)
     }
 }
